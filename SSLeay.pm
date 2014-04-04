46d91a3e2e4bce5ed937341962e8802c80f3a256
A. Sinan Unur <nanis@cpan.org>
2014-04-04 08:44:19 -0400

    Allow incpath and lbpath command line arguments to Makefile.PL

--- SSLeay.pm
+++ SSLeay.pm
@@ -2,7 +2,7 @@ package Crypt::SSLeay;
 
 use strict;
 use vars '$VERSION';
-$VERSION = '0.65_04';
+$VERSION = '0.65_05';
 
 eval {
     require XSLoader;
--- openssl-version.c
+++ openssl-version.c
@@ -3,5 +3,6 @@
 
 int main(void) {
     puts(OPENSSL_VERSION_TEXT);
+    printf("%8lx\n", OPENSSL_VERSION_NUMBER);
     return 0;
 }
